a076fb47a08280a32693fc09be33c773dcc1f370
timaci <129782864+timaci@users.noreply.github.com>
2023-04-29 20:22:49 -0300

    Componentes de Estado
    Thiago César - 3º Informática

--- main.dart
+++ main.dart
@@ -1,39 +1,50 @@
 import 'package:flutter/material.dart';
 
-main(){
-  runApp(ComponenteInicial());
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
 }
 
-class ComponenteInicial extends StatelessWidget{
+class _MyAppState extends State<MyApp> {
+  int contador = 0;
+  List<Map<String, dynamic>> perguntas = [    {      'pergunta': 'Qual é a capital do Brasil?',      'resposta': 'Brasília',    },    {      'pergunta': 'Quantos estados tem o Brasil?',      'resposta': '26',    },    {      'pergunta': 'Qual é o maior país do mundo?',      'resposta': 'Rússia',    }  ];
 
-  void enviar(){
-    print("Enviar");}
-      void cancelar(){
-    print("Cancelar");}
-      void salvar(){
-    print("Salvar");}
-  
-
-Widget build(BuildContext context){
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("treinando programação"),
-
+          title: Text('Exemplo StatefulWidget - Thiago César'),
         ),
-        body:Column (children: [
-           Text("Aprendendo"),
-           Text("Programação"),
-           Text("Flutter"),
-        
-        Column (children: [
-          ElevatedButton(onPressed: enviar, child: Text("Enviar")),
-          ElevatedButton(onPressed: cancelar, child: Text("Cancelar")),
-          ElevatedButton(onPressed: salvar, child: Text("Salvar")),
-        ],)
-          ],
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                perguntas[contador]['pergunta'],
+                style: TextStyle(fontSize: 24),
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Resposta: ${perguntas[contador]['resposta']}',
+                style: TextStyle(fontSize: 18),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    contador = (contador + 1) % perguntas.length;
+                  });
+                },
+                child: Text('Próxima pergunta'),
+              ),
+            ],
+          ),
         ),
-       ), 
-      );
+      ),
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
