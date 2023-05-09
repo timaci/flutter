885b1d811c0cd1024a96799d7a4347fc9fd2efb1
timaci <129782864+timaci@users.noreply.github.com>
2023-05-09 19:40:59 -0300

    componente Reposta
    Thiago César Resende Rodrigues 3º Informática

--- main.dart
+++ main.dart
@@ -1,50 +1,32 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
+void main() {
+  runApp(MyApp(text: 'Press me'));
 }
 
-class _MyAppState extends State<MyApp> {
-  int contador = 0;
-  List<Map<String, dynamic>> perguntas = [    {      'pergunta': 'Qual é a capital do Brasil?',      'resposta': 'Brasília',    },    {      'pergunta': 'Quantos estados tem o Brasil?',      'resposta': '26',    },    {      'pergunta': 'Qual é o maior país do mundo?',      'resposta': 'Rússia',    }  ];
+class MyApp extends StatelessWidget {
+  final String text;
+
+  const MyApp({Key? key, required this.text}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Exemplo StatefulWidget - Thiago César'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                perguntas[contador]['pergunta'],
-                style: TextStyle(fontSize: 24),
-              ),
-              SizedBox(height: 20),
-              Text(
-                'Resposta: ${perguntas[contador]['resposta']}',
-                style: TextStyle(fontSize: 18),
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    contador = (contador + 1) % perguntas.length;
-                  });
-                },
-                child: Text('Próxima pergunta'),
-              ),
-            ],
-          ),
-        ),
+    return Directionality(
+      textDirection: TextDirection.ltr,
+      
+      child: TextButton(
+        onPressed: () {},
+        child: Text('botão'),
+         style: TextButton.styleFrom(
+           minimumSize: Size(50, 30),
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(10), // Define o raio do botão
+    ),
+    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+  ),
+  
       ),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
+
