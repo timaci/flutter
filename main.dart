d58f62e12dac9f678e38df21abfff7d94242ad0c
timaci <129782864+timaci@users.noreply.github.com>
2023-05-29 15:03:23 -0300

    aula 29/05
    Thiago César

--- main.dart
+++ main.dart
@@ -1,66 +1,101 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(QuestionnaireApp());
 }
 
-class MyApp extends StatefulWidget {
+class QuestionnaireApp extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() => _MyAppState();
+  _QuestionnaireAppState createState() => _QuestionnaireAppState();
 }
 
-class _MyAppState extends State<MyApp> {
-  Color _backgroundColor = Colors.white; // Cor inicial de fundo
+class _QuestionnaireAppState extends State<QuestionnaireApp> {
+  final List<Map<String, dynamic>> questions = [
+    {
+      'pergunta': 'Qual é a capital do Brasil?',
+      'alternativas': ['Rio de Janeiro', 'Brasília', 'São Paulo', 'Belo Horizonte'],
+      'resposta': '',
+    },
+    {
+      'pergunta': 'Quem descobriu o Brasil?',
+      'alternativas': ['Pedro Álvares Cabral', 'Cristóvão Colombo', 'Fernão de Magalhães', 'Bartolomeu Dias'],
+      'resposta': '',
+    },
+    {
+      'pergunta': 'Quantos planetas existem no sistema solar?',
+      'alternativas': ['7', '8', '9', '10'],
+      'resposta': '',
+    },
+  ];
 
-  void _changeToBlue() {
-    setState(() {
-      // Altera a cor de fundo para azul
-      _backgroundColor = Colors.lightBlue;
-    });
-  }
+  int currentQuestionIndex = 0;
 
-  void _changeToGreen() {
-    setState(() {
-      // Altera a cor de fundo para verde
-      _backgroundColor = Colors.lightGreen;
-    });
-  }
-
-void _changeToBlack() {
-    setState(() {
-      // Altera a cor de fundo para verde
-      _backgroundColor = Colors.black;
-    });
-  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Questionário',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Botões de Mudança de Cor'),
+          title: Text('Questionário'),
         ),
-        body: Container(
-          color: _backgroundColor,
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  onPressed: _changeToBlue,
-                  child: Text('TELA AZUL'),
+        body: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                questions[currentQuestionIndex]['pergunta']!,
+                style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 16.0),
+              Column(
+                children: List<Widget>.generate(
+                  questions[currentQuestionIndex]['alternativas'].length,
+                  (index) => RadioListTile(
+                    title: Text(questions[currentQuestionIndex]['alternativas'][index]),
+                    value: questions[currentQuestionIndex]['alternativas'][index],
+                    groupValue: questions[currentQuestionIndex]['resposta'],
+                    onChanged: (value) {
+                      setState(() {
+                        questions[currentQuestionIndex]['resposta'] = value;
+                      });
+                    },
+                  ),
                 ),
-                SizedBox(height: 16),
-                ElevatedButton(
-                  onPressed: _changeToGreen,
-                  child: Text('TELA VERDE'),
+              ),
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    if (currentQuestionIndex < questions.length - 1) {
+                      currentQuestionIndex++;
+                    } else {
+                      // Questionário concluído
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: Text('Questionário concluído'),
+                            content: Text('O questionário foi respondido.'),
+                            actions: [
+                              TextButton(
+                                child: Text('OK'),
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    }
+                  });
+                },
+                child: Text(
+                  currentQuestionIndex < questions.length - 1 ? 'Enviar' : 'Finalizar questionário',
                 ),
-                SizedBox(height: 16),
-                ElevatedButton(
-                  onPressed: _changeToBlack,
-                  child: Text('TELA PRETA'),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
