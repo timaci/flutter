6bc7ced50ddd434e2c4ad15e348d2495171b83d4
timaci <129782864+timaci@users.noreply.github.com>
2023-06-26 14:23:41 -0300

    Avaliação PDM 26/06
    Thiago César Resende Rodrigues

--- main.dart
+++ main.dart
@@ -1,104 +1,117 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(QuestionnaireApp());
+  runApp(QuestionarioApp());
 }
 
-class QuestionnaireApp extends StatefulWidget {
-  @override
-  _QuestionnaireAppState createState() => _QuestionnaireAppState();
-}
-
-class _QuestionnaireAppState extends State<QuestionnaireApp> {
-  final List<Map<String, dynamic>> questions = [
-    {
-      'pergunta': 'Qual é a capital do Brasil?',
-      'alternativas': ['Rio de Janeiro', 'Brasília', 'São Paulo', 'Belo Horizonte'],
-      'resposta': '',
-    },
-    {
-      'pergunta': 'Quem descobriu o Brasil?',
-      'alternativas': ['Pedro Álvares Cabral', 'Cristóvão Colombo', 'Fernão de Magalhães', 'Bartolomeu Dias'],
-      'resposta': '',
-    },
-    {
-      'pergunta': 'Quantos planetas existem no sistema solar?',
-      'alternativas': ['7', '8', '9', '10'],
-      'resposta': '',
-    },
-  ];
-
-  int currentQuestionIndex = 0;
-
+class QuestionarioApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Questionário',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Questionário'),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Questionario(),
+    );
+  }
+}
+
+class Questionario extends StatefulWidget {
+  @override
+  _QuestionarioState createState() => _QuestionarioState();
+}
+
+class _QuestionarioState extends State<Questionario> {
+  List<String> respostas = [];
+  int perguntaAtual = 0;
+
+  List<Map<String, dynamic>> perguntas = [
+    {
+      'pergunta': 'Qual é a capital do Brasil?',
+      'respostas': ['São Paulo', 'Rio de Janeiro', 'Brasília', 'Salvador'],
+      'respostaCorreta': 'Brasília',
+    },
+    {
+      'pergunta': 'Quem descobriu o Brasil?',
+      'respostas': ['Pedro Álvares Cabral', 'Cristóvão Colombo', 'Américo Vespúcio', 'Fernão de Magalhães'],
+      'respostaCorreta': 'Pedro Álvares Cabral',
+    },
+    {
+      'pergunta': 'Qual é a cor do céu?',
+      'respostas': ['Vermelho', 'Azul', 'Verde', 'Amarelo'],
+      'respostaCorreta': 'Azul',
+    },
+  ];
+
+  void responder(String resposta) {
+    setState(() {
+      respostas.add(resposta);
+      perguntaAtual++;
+    });
+
+    if (perguntaAtual >= perguntas.length) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => Resultado(respostas),
         ),
-        body: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Text(
-                questions[currentQuestionIndex]['pergunta']!,
-                style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 16.0),
-              Column(
-                children: List<Widget>.generate(
-                  questions[currentQuestionIndex]['alternativas'].length,
-                  (index) => RadioListTile(
-                    title: Text(questions[currentQuestionIndex]['alternativas'][index]),
-                    value: questions[currentQuestionIndex]['alternativas'][index],
-                    groupValue: questions[currentQuestionIndex]['resposta'],
-                    onChanged: (value) {
-                      setState(() {
-                        questions[currentQuestionIndex]['resposta'] = value;
-                      });
-                    },
-                  ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Questionário'),
+      ),
+      body: perguntaAtual < perguntas.length
+          ? Column(
+              children: [
+                Text(
+                  perguntas[perguntaAtual]['pergunta'] as String,
+                  style: TextStyle(fontSize: 18),
                 ),
+                SizedBox(height: 16),
+                ...((perguntas[perguntaAtual]['respostas'] as List<dynamic>).map((resposta) {
+                  return ElevatedButton(
+                    onPressed: () => responder(resposta as String),
+                    child: Text(resposta as String),
+                  );
+                }).toList()),
+              ],
+            )
+          : Center(
+              child: Text(
+                'Questionário terminado!',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
               ),
-              SizedBox(height: 16.0),
-              ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    if (currentQuestionIndex < questions.length - 1) {
-                      currentQuestionIndex++;
-                    } else {
-                      // Questionário concluído
-                      showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return AlertDialog(
-                            title: Text('Questionário concluído'),
-                            content: Text('O questionário foi respondido.'),
-                            actions: [
-                              TextButton(
-                                child: Text('OK'),
-                                onPressed: () {
-                                  Navigator.of(context).pop();
-                                },
-                              ),
-                            ],
-                          );
-                        },
-                      );
-                    }
-                  });
-                },
-                child: Text(
-                  currentQuestionIndex < questions.length - 1 ? 'Enviar' : 'Finalizar questionário',
-                ),
-              ),
-            ],
-          ),
-        ),
+            ),
+    );
+  }
+}
+
+class Resultado extends StatelessWidget {
+  final List<String> respostas;
+
+  Resultado(this.respostas);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Resultado do Questionário'),
+      ),
+      body: ListView.builder(
+        itemCount: respostas.length,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            title: Text('Resposta ${index + 1}: ${respostas[index]}'),
+          );
+        },
       ),
     );
   }
 }
+
